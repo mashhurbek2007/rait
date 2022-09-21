b0f9ad44a538fa8277ca8f8d03d16e53b650962f
Mashhurbek <104623162+mashhurbek2007@users.noreply.github.com>
2022-09-21 16:07:52 +0600

    Add files via upload

---
+++ Rait/website/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+
+def home(request):
+    rate = RateStars.objects.filter(score=3).order_by("?").first()
+    context = {
+        "rate": rate,
+    }
+    return render(request, 'home.html', context)
+
+def rate_image(request):
+	if request.method == "POST":
+		el_id = request.POST.get("el_id")
+		val = request.POST.get("val")
+		rate = RateStars.objects.get(id=el_id)
+		rate.score = val
+		rate.save()
+		return JsonResponse({"success": "true", "score": val}, safe=False)
+	return JsonResponse({"success": "false"})
